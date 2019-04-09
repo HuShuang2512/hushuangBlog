ec1dae429487ba189e3c87d73600d252bf39cb0e
HuShuang2512 <985974698@qq.com>
2019-04-09 13:49:10 +0800

    init

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host:"127.0.0.1",
+        host:"192.168.0.8",
         post: "3306",
         user: "root",
         password: "123456789",
